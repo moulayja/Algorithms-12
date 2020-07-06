6d3b1235de1a224fbe16794940b9a2fdf5e62aa6
Jamal <moulayjam@gmail.com>
2020-07-06 06:59:06 -0400

    RGB to Hex Color Converter[Solution]

--- RGB to Hex Color Converter.js
+++ RGB to Hex Color Converter.js
@@ -2,3 +2,10 @@
 /*
 Create a function that converts color in RGB format to Hex format.
 */
+
+const rgbToHex = color => {
+	return "#" + color
+		.match(/\d+/g)
+		.map(v => (+v).toString(16).padStart(2, "0"))
+		.join("");
+}
